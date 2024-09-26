4ccea8378bb59382b029977ce7520d2644aa9cde
anacarolliny <acss2011@hotmail.com>
2024-09-26 18:59:01 -0300

    feat: ADD NOVA ROTA

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 // import Login from './pages/Login';
 // import Register from './pages/Register';
 import AdicionarProdutos from './pages/adicionar-produto';
@@ -7,9 +7,11 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Redireciona a rota padrão para a página de adicionar produtos */}
+        <Route path="/" element={<Navigate to="/adicionar-produto" replace />} />
         {/* <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} /> */}
-        <Route path='/adicionar-produto' element={<AdicionarProdutos /> } />
+        <Route path='/adicionar-produto' element={<AdicionarProdutos />} />
       </Routes>
     </Router>
   );
